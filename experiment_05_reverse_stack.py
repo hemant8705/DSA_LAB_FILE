74069a20f313fc22485d66e64ff0a3ec4c5ea210
Hemant Saini <hemantsaini872005@gmail.com>
2025-11-30 22:30:00 +0530

    Create experiment_05_reverse_stack.py

---
+++ experiment_05_reverse_stack.py
@@ -0,0 +1,11 @@
+# Experiment 05: Reverse a String using Stack
+
+def reverse_string(s):
+    stack = []
+    for ch in s:
+        stack.append(ch)
+
+    rev = ""
+    while stack:
+        rev += stack.pop()
+    return rev
